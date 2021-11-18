6d4c89d9f2514e354f3b7ef27db5a002fa785714
nicole <000431652@student.vcc.ca>
2021-11-18 13:08:18 -0800

    working calendar

--- src/Screens/CalendarScreen.js
+++ src/Screens/CalendarScreen.js
@@ -13,32 +13,31 @@ const CalendarScreen = (navigation) => {
 
             <Calendar
                 // Initially visible month. Default = Date()
-                current={'2012-03-01'}
+                current={'2021-11-01'}
                 // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                minDate={'2012-05-10'}
+                //if specific dates are inside it is greyed out
+                minDate={''}
                 // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                maxDate={'2012-05-30'}
+                maxDate={''}
                 // Handler which gets executed on day press. Default = undefined
                 onDayPress={(day) => {console.log('selected day', day)}}
                 // Handler which gets executed on day long press. Default = undefined
                 onDayLongPress={(day) => {console.log('selected day', day)}}
                 // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-                monthFormat={'yyyy MM'}
+                monthFormat={'MMM yyyy'}
                 // Handler which gets executed when visible month changes in calendar. Default = undefined
                 onMonthChange={(month) => {console.log('month changed', month)}}
-                // Hide month navigation arrows. Default = false
-                hideArrows={true}
-                // Replace default arrows with custom ones (direction can be 'left' or 'right')
-                renderArrow={(direction) => (<Arrow/>)}
+                
                 // Do not show days of other months in month page. Default = false
-                hideExtraDays={true}
+                hideExtraDays={false}
                 // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
                 // day from another month that is visible in calendar page. Default = false
-                disableMonthChange={true}
+                disableMonthChange={false}
                 // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
-                firstDay={1}
+                //firstDay = {0} starts from Sunday
+                firstDay={0}
                 // Hide day names. Default = false
-                hideDayNames={true}
+                hideDayNames={false}
                 // Show week numbers to the left. Default = false
                 showWeekNumbers={true}
                 // Handler which gets executed when press arrow icon left. It receive a callback can go back month
@@ -46,9 +45,9 @@ const CalendarScreen = (navigation) => {
                 // Handler which gets executed when press arrow icon right. It receive a callback can go next month
                 onPressArrowRight={addMonth => addMonth()}
                 // Disable left arrow. Default = false
-                disableArrowLeft={true}
+                disableArrowLeft={false}
                 // Disable right arrow. Default = false
-                disableArrowRight={true}
+                disableArrowRight={false}
                 // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
                 disableAllTouchEventsForDisabledDays={true}
                 // Replace default month and year title with custom one. the function receive a date as parameter

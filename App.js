fcc1096f8085e4c21f5082f9ad9a14adf66ae961
Rayyan Haroon <000427071@student.vcc.ca>
2021-12-04 18:03:22 -0800

    code for tab icons added, but nothing is appearing. commented out currently

--- App.js
+++ App.js
@@ -49,7 +49,30 @@ const HomeStackScreen = () => (
 )
 
 const TabScreen = () => (
-  <Tabs.Navigator screenOptions={{headerShown:false}} >
+  <Tabs.Navigator screenOptions={{headerShown:false}}>
+  {/* screenOptions={({route})=>({
+    tabBarIcon:({focused, size, color})=>{
+      let iconName;
+      if(route.name==='HomeScreen') {
+        iconName='home';
+        size = focused?25:20;
+        color = focused?'#f0f':'#555';
+      } else if(route.name==='AddEvent') {
+        iconName='calendar';
+        size = focused?25:20;
+      } else if(route.name==='ProfileScreen') {
+        iconName='user';
+        size = focused?25:20;
+      }
+      return(
+        <FontAwesome5
+          name={iconName}
+          size={size}
+          color={color}
+        />
+      )
+    }
+  })}} */}
     <Tabs.Screen name="Home" component={HomeStackScreen} /> 
     <Tabs.Screen name="Create an Event" component={AddEventScreen}/> 
     <Tabs.Screen name="My Profile" component={ProfileScreen} />

d04ed25803b5de63ba2ed71912462c090b207b86
Rayyan Haroon <000427071@student.vcc.ca>
2021-12-09 13:35:58 -0800

    nav bar icons

--- Screens/AddEventScreen.js
+++ Screens/AddEventScreen.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 
 const AddEventScreen = ({navigation}) => {
 
-    const [eventName, onChangeEventName] = React.useState('');
-        return (
+    const [eventName, onChangeEventName] = React.useState('')
+    const [description, onChangeDesc] = React.useState('')
 
-            <View style={styles.container}>
-
-                <Text style={styles.eventTitle}>Enter event name:</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventTitle}>Enter event name:</Text>
 
                 <TextInput
                     style={styles.eventInput}
@@ -16,8 +16,22 @@ const AddEventScreen = ({navigation}) => {
                     value={eventName}
                     placeholder={'Event Name'}
                 />
-            </View>
-        )            
+                <TextInput
+                    style={styles.descInput}
+                    onChangeText={onChangeDesc}
+                    value={description}
+                    placeholder={'Description'}
+                />
+                
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={styles.saveBtn}
+                        onPress={() => navigation.navigate("EventScreen")}>
+                        <Text style={styles.buttonText}>Save</Text>
+                    </TouchableOpacity>
+                </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -44,6 +58,31 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         borderRadius: 10
     },
+    descInput: {
+        width: "90%",
+        backgroundColor: '#fff',
+        padding: 15,
+        marginBottom: 15,
+        borderRadius: 10
+    },
+    buttonContainer: {
+        // stacks the components in the container 
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '90%'
+    },
+    saveBtn: {
+        backgroundColor: '#ffd700',
+        padding: 15,
+        width: "45%",
+        marginBottom: 10,
+        borderRadius: 10,
+    },
+    buttonText: {
+        fontSize: 18,
+        textAlign:'center',
+        borderRadius: 10
+    }
   });
 
 export default AddEventScreen;
